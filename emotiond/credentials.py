f5a3943ec0cebd7c7b08587012f2e3a334ddf592
root <root@localhost.localdomain>
2021-03-02 18:46:37 -0500

    fixing stuff

--- emotiond/credentials.py
+++ emotiond/credentials.py
@@ -1,3 +1,3 @@
-bot_token = 1567284648:AAEm7GhdNqpG7ggpPwOQy0ySvFH4T3PFY7s
+bot_token = "1567284648:AAEm7GhdNqpG7ggpPwOQy0ySvFH4T3PFY7s"
 bot_user_name = emotiond_bot
 URL = https://emotiond.herokuapp.com/
